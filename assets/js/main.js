c4d16bbdb2433a64e79c2ac5480d084599fd9fe2
Acknology CI <githubci@acknology.co>
2022-03-05 15:04:59 +0530

    ✨ Add AnimateOnScroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 // ########
 // ### AnimateOnScroll - AoS : Initilize
 // ########
-AOS.init();
+AOS.init({
+  // Global settings:
+  delay: 100, // values from 0 to 3000, with step 50ms
+  // once: true, // whether animation should happen only once - while scrolling down
+});
 
 // ########
 // ### Scroll to top
